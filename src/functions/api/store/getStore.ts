52c65f34d2b7e8e3776acb714533fc8131fc0870
natreeum <projectkmscom@gmail.com>
2023-04-13 15:23:28 +0900

    feat : utilize response message

--- src/functions/api/store/getStore.ts
+++ src/functions/api/store/getStore.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
+import { responseMsg } from '../../../../utils/responseMsg';
 const getStore = async (req: Request, res: Response) => {
   const { address } = req.params;
-  if (!address) return res.status(400).send('Bad Request');
+  if (!address) {
+    return res.status(400).send(responseMsg[400]);
+  }
   return res.send(`GET /api/store/${address}`);
 };
 
