f88d42313a4b9b82b5f94da47df6c66e6e55ff39
natreeum <projectkmscom@gmail.com>
2023-04-21 14:26:31 +0900

    feat : create record

--- utils/query.ts
+++ utils/query.ts
@@ -1,7 +1,19 @@
 import mysqlQueryPromise from './mysql';
+import { createRecord } from './queryModules';
 
 const main = async () => {
-  const res = await mysqlQueryPromise(`select * from Store where name='123'`);
+  // const res = await mysqlQueryPromise(`select * from Store`);
+  const res = await createRecord({
+    table: `Store`,
+    data: {
+      name: `545`,
+      image: `a.jpg`,
+      description: `test desc`,
+      market_address: `rkdska`,
+      open_time: `1200`,
+      location_address: `rkdska`,
+    },
+  });
   console.log(res);
 };
 
