16efa71044343a863b28ed52c0a798a665e07302
natreeum <projectkmscom@gmail.com>
2023-05-24 10:43:49 +0900

    gitignore add : .DS_Store

--- build/utils/responseMsg.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.responseMsg = void 0;
-exports.responseMsg = {
-    200: 'Success',
-    201: 'Created',
-    400: 'Bad Request',
-    500: 'Internal Error',
-};
