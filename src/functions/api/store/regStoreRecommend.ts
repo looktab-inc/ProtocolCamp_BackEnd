a36084056daf98b4aeb1b355c7f6dd6fed24a571
natreeum <projectkmscom@gmail.com>
2023-05-25 15:12:53 +0900

    feat : recommend registeration

--- src/functions/api/store/regStoreRecommend.ts
+++ src/functions/api/store/regStoreRecommend.ts
@@ -3,7 +3,8 @@ import { responseMsg } from "../../../../utils/responseMsg";
 import { createRecord } from "../../../../utils/queryModules";
 
 export default async function regRecData(req: Request, res: Response) {
-  const { user_id, store_id, comment, img1, img2, img3, img4, img5 } = req.body;
+  const { user_id, comment, img1, img2, img3, img4, img5 } = req.body;
+  const store_id = req.params.id;
   if (!(user_id && store_id && comment && img1))
     return res.status(400).send(responseMsg[400]);
 
