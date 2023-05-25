40676b0e0c4dc9c458de402bd96b74159142b7f1
TaeGit <tgsong827@gmail.com>
2023-05-25 19:00:04 +0900

    update contract info

--- utils/contract/tinjiContract.ts
+++ utils/contract/tinjiContract.ts
@@ -63,7 +63,8 @@ export class TinjiContract {
   // bank: 0.009, client: 0.001
   async withdrawForBurned(
     bankAccountAddress: web3.PublicKey,
-    clientAddress: web3.PublicKey
+    clientAddress: web3.PublicKey,
+    recommenderAddress: web3.PublicKey,
   ): Promise<string> {
     const pdaAuthPubKey = this.getPdaAuthPubkey(bankAccountAddress);
     const solVaultPubkey = this.getSolVaultPubkey(pdaAuthPubKey);
@@ -77,6 +78,7 @@ export class TinjiContract {
         solVault: solVaultPubkey,
         systemProgram: web3.SystemProgram.programId,
         clientAccount: clientAddress,
+        recommenderAccount: recommenderAddress,
       })
       .signers([this.bankWallet])
       .rpc();
@@ -87,7 +89,8 @@ export class TinjiContract {
   // bank: 0.01, client: 0
   async withdrawForExpired(
     bankAccountAddress: web3.PublicKey,
-    clientAddress: web3.PublicKey
+    clientAddress: web3.PublicKey,
+    recommenderAddress: web3.PublicKey,
   ): Promise<string> {
     const pdaAuthPubKey = this.getPdaAuthPubkey(bankAccountAddress);
     const solVaultPubkey = this.getSolVaultPubkey(pdaAuthPubKey);
@@ -101,6 +104,7 @@ export class TinjiContract {
         solVault: solVaultPubkey,
         systemProgram: web3.SystemProgram.programId,
         clientAccount: clientAddress,
+        recommenderAccount: recommenderAddress,
       })
       .signers([this.bankWallet])
       .rpc();
@@ -111,7 +115,8 @@ export class TinjiContract {
   // bank: 0.001, client: 0.009
   async withdrawForVerified(
     bankAccountAddress: web3.PublicKey,
-    clientAddress: web3.PublicKey
+    clientAddress: web3.PublicKey,
+    recommenderAddress: web3.PublicKey,
   ): Promise<string> {
     const pdaAuthPubKey = this.getPdaAuthPubkey(bankAccountAddress);
     const solVaultPubkey = this.getSolVaultPubkey(pdaAuthPubKey);
@@ -125,6 +130,7 @@ export class TinjiContract {
         solVault: solVaultPubkey,
         systemProgram: web3.SystemProgram.programId,
         clientAccount: clientAddress,
+        recommenderAccount: recommenderAddress,
       })
       .signers([this.bankWallet])
       .rpc();
