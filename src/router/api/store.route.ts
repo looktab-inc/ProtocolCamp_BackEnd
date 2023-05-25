d727d76d4254734383cb8c1679ba87b6dc071381
natreeum <projectkmscom@gmail.com>
2023-05-25 15:12:53 +0900

    feat : recommend registeration

--- src/router/api/store.route.ts
+++ src/router/api/store.route.ts
@@ -7,7 +7,7 @@ import getStoreReview from "../../functions/api/store/getStoreReview";
 import newReview from "../../functions/api/store/newReview";
 import postLike from "../../functions/api/store/postLike";
 import storeRecommend from "../../functions/api/store/getStoreRecommend";
-import regRecData from "../../functions/api/store/regStoreReccomend";
+import regRecData from "../../functions/api/store/regStoreRecommend";
 
 const router: Router = express.Router();
 
@@ -18,9 +18,9 @@ router.route("/:id/rec").get(storeRecommend);
 
 // POST
 router.route("/").post(postStore);
-router.route("/review").post(newReview);
+router.route("/:id/review").post(newReview);
 router.route("/like").post(postLike);
-router.route("/recreg").post(regRecData);
+router.route("/:id/recreg").post(regRecData);
 
 // PATCH
 router.route("/").patch(patchStore);
