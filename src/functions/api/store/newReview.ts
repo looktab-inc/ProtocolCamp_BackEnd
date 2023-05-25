1018c3a7f10dd69b52e6ef32a8d068d09a087ad3
natreeum <projectkmscom@gmail.com>
2023-05-25 18:53:28 +0900

    feat : cool things

--- src/functions/api/store/newReview.ts
+++ src/functions/api/store/newReview.ts
@@ -1,28 +1,41 @@
 import { Request, Response } from "express";
-import { createRecord } from "../../../../utils/queryModules";
+import { createRecord, findRecord } from "../../../../utils/queryModules";
 import { responseMsg } from "../../../../utils/responseMsg";
 
 export default async function (req: Request, res: Response) {
   const store_id = req.params.id;
-  const { user_id, content, img1, img2, img3 } = req.body;
+  const { user_id, content, img1, img2, img3, like_id, summary } = req.body;
 
-  if (!(store_id && user_id && content))
+  if (!(store_id && user_id && content && like_id && summary))
     return res.status(400).send(responseMsg[400]);
 
   try {
+    const [likeData] = await findRecord({
+      table: "Like",
+      data: { id: like_id },
+    });
+
+    const [userData] = await findRecord({
+      table: "Users",
+      data: { id: user_id },
+    });
+
+    const nft_address = likeData.nft_address;
+
+    // 여기에 withdraw logic
+
     const db_res = await createRecord({
       table: "Review",
       data: {
         store_id,
         user_id,
+        summary,
         content,
         img1,
         img2,
         img3,
       },
     });
-
-    // reward 주세여
 
     return res.status(201).send(db_res);
   } catch (e) {
--- src/functions/api/store/postLike.ts
+++ src/functions/api/store/postLike.ts
@@ -1,54 +1,68 @@
-import { Request, Response } from 'express';
-import { responseMsg } from '../../../../utils/responseMsg';
+import { Request, Response } from "express";
+import { responseMsg } from "../../../../utils/responseMsg";
 import {
   createRecord,
   findRecord,
   updateRecord,
-} from '../../../../utils/queryModules';
+} from "../../../../utils/queryModules";
 
 export default async function (req: Request, res: Response) {
-  const { store_id, user_id } = req.body;
+  const { store_id, user_id, recDataId } = req.body;
 
   // exception handling : Bad Request
-  if (!(store_id && user_id)) return res.status(400).send(responseMsg[400]);
+  if (!(store_id && user_id && recDataId))
+    return res.status(400).send(responseMsg[400]);
 
   try {
-    // create record
-    const db_create_res = await createRecord({
-      table: 'Like',
-      data: {
-        user_id,
-        store_id,
-      },
+    // RecData load
+    const [recData] = await findRecord({
+      table: "RecommendData",
+      data: { id: recDataId },
     });
 
-    // update record
+    // user data load
     const [db_read_res] = await findRecord({
-      table: 'Users',
+      table: "Users",
       data: { id: user_id },
     });
 
+    // IMG upload Logic
+    const imgUrl = "hththth";
+
+    // NFT Mint Logic
+    const nft_address = "0x1234";
+
+    // create record
+    const db_create_res = await createRecord({
+      table: "Like",
+      data: {
+        user_id,
+        store_id,
+        nft_address,
+      },
+    });
+
     const db_update_res = await updateRecord({
-      table: 'Users',
+      table: "Users",
       data: { like_count: db_read_res.like_count + 1 },
       where: { id: user_id },
     });
   } catch (e) {
     console.log(e);
-    return res.status(500).send('Internal Error');
+    return res.status(500).send("Internal Error");
   }
 
   try {
     const [user] = await findRecord({
-      table: 'Users',
+      table: "Users",
       data: { id: user_id },
     });
     const like_count = user.like_count;
     return res.status(200).send({
-      status: 'success',
+      status: "success",
       like_count,
     });
   } catch (e) {
-    return res.status(500).send('Reading user data by user id failed');
+    return res.status(500).send("Reading user data by user id failed");
   }
 }
