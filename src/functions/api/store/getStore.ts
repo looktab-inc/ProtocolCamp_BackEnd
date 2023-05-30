fa10378fd73269989b39ec9b777fb36047464c62
natreeum <projectkmscom@gmail.com>
2023-05-30 10:59:10 +0900

    feat : store 정보가져오기 수정

--- src/functions/api/store/getStore.ts
+++ src/functions/api/store/getStore.ts
@@ -7,7 +7,7 @@ const getStore = async (req: Request, res: Response) => {
     return res.status(400).send(responseMsg[400]);
   }
   // get store data by address
-  const getStoreRes = await findRecord({ table: "Store", data: { id } });
+  const [getStoreRes] = await findRecord({ table: "Store", data: { id } });
   if (!getStoreRes) return res.status(500).send("Error");
   if (getStoreRes.length === 0) return res.send(`No store with id ${id}`);
 
