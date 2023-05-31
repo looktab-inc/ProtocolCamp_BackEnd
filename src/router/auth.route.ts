1dd31204d73f15dbfc0403e1efcdc77902214218
natreeum <projectkmscom@gmail.com>
2023-05-31 13:41:21 +0900

    asdf

--- src/router/auth.route.ts
+++ src/router/auth.route.ts
@@ -1,13 +1,17 @@
-import express, { Router } from 'express';
-import fs from 'fs';
+import express, { Router } from "express";
+import fs from "fs";
 
 const router: Router = express.Router();
 
-const routersDir = __filename.split('.')[0];
+router.get("/", (req, res) => {
+  res.send("auth");
+});
+
+const routersDir = __filename.split(".")[0];
 fs.readdirSync(routersDir)
-  .filter((f) => f.indexOf('.') !== 0 && f.slice(-9) === '.route.ts')
+  .filter((f) => f.indexOf(".") !== 0 && f.slice(-9) === ".route.ts")
   .forEach((r) =>
-    router.use(`/${r.split('.')[0]}`, require(`${routersDir}/${r}`).default)
+    router.use(`/${r.split(".")[0]}`, require(`${routersDir}/${r}`).default)
   );
 
 export default router;
