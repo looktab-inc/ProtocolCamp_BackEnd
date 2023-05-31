4c07e60b8ae4945882daa42ae5125ddc77d37869
natreeum <projectkmscom@gmail.com>
2023-05-31 14:03:57 +0900

    asdf

--- utils/auth/apikeyCheck.ts
+++ utils/auth/apikeyCheck.ts
@@ -1,13 +1,9 @@
 import { Request } from "express";
 import uuidAPIKey from "uuid-apikey";
-import dotenv from "dotenv";
-dotenv.config();
 
 export default function apiAuth(req: Request) {
   try {
     const apikey = req.headers.authorization;
-    console.log(apikey);
-    console.log(process.env.UUID);
     const keyRes = uuidAPIKey.check(apikey || "", process.env.UUID || "");
     const checkRes = !apikey || keyRes;
     return checkRes;
