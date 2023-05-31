ae3ffb20f74626a51d4c85f7019bb035670e1da1
natreeum <projectkmscom@gmail.com>
2023-05-31 13:57:52 +0900

    asdf

--- utils/auth/apikeyCheck.ts
+++ utils/auth/apikeyCheck.ts
@@ -1,10 +1,10 @@
-import { Request } from 'express';
-import uuidAPIKey from 'uuid-apikey';
+import { Request } from "express";
+import uuidAPIKey from "uuid-apikey";
 
 export default function apiAuth(req: Request) {
-  const apikey = req.headers.authorization;
-  const keyRes = uuidAPIKey.check(apikey || '', process.env.UUID || '');
   try {
+    const apikey = req.headers.authorization;
+    const keyRes = uuidAPIKey.check(apikey || "", process.env.UUID || "");
     const checkRes = !apikey || keyRes;
     return checkRes;
   } catch (e) {
